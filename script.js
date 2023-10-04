79e897952d017425bd15b5c8dd5dfdf48006cb5e
Alfian <titanalfian112@gmail.com>
2023-10-05 00:05:41 +0700

    new final

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function parseData(people) {
   // console.log(people);
   const result = people.response.map((person) => {
     const parsedPerson = {
-      country: person.continent,
+      country: person.country,
       active: person.cases.active,
       new: person.cases.new,
       recovered: person.cases.recovered,
@@ -36,33 +36,29 @@ function parseData(people) {
   myLoading = false;
   console.log(result);
 }
-
 function button() {
   const myInput = document.getElementById("search");
   const currentValue = myInput.value;
   const result = globalPeople.find((el) => {
     return el.country == currentValue;
   });
-  console.log(result);
+  const active = document.getElementById("active");
+  const cases = document.getElementById("cases");
+  const deaths = document.getElementById("deaths");
+  const New = document.getElementById("new");
+  const recovered = document.getElementById("recovered");
+  const tests = document.getElementById("tests");
+  console.log(active, cases, deaths, New, recovered, tests);
+
+  const HasilAkhir = [
+    (active.innerText = result.active),
+    (cases.innerText = result.cases),
+    (deaths.innerText = result.deaths),
+    (New.innerText = result.new),
+    (recovered.innerText = result.recovered),
+    (tests.innerText = result.tests),
+  ];
+  return HasilAkhir;
 }
 
-// const active = document.getElementById("active");
-// const cases = document.getElementById("cases");
-// const deaths = document.getElementById("deaths");
-// const New = document.getElementById("new");
-// const recovered = document.getElementById("recovered");
-// const tests = document.getElementById("tests");
-// console.log(active, cases, deaths, New, recovered, tests);
-
-// const HasilAkhir = [
-//   (active.innerText = hasil.active),
-//   (cases.innerText = hasil.cases),
-//   (deaths.innerText = hasil.deaths),
-//   (New.innerText = hasil.new),
-//   (recovered.innerText = hasil.recovered),
-//   (tests.innerText = hasil.tests),
-// ];
-// if ((HasilAkhir.innerText = null)) {
-//   innerText = 0;
-// }
-// return HasilAkhir;
+window.open(alert("diharapkan menggunakan huruf awal besar"));
